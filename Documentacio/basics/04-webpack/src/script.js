8ba50f15f039044b8974d17787daad701fac5416
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-01-13 09:58:18 +0100

    ThreeJS

--- Documentacio/basics/04-webpack/src/script.js
+++ Documentacio/basics/04-webpack/src/script.js
@@ -2,4 +2,32 @@ import './style.css';
 import * as THREE from 'three';
 
 console.log('Hello Webpack & ThreeJS!');
-console.log(THREE.PerspectiveCamera);
+console.log(THREE);
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objecte (Mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = -1;
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas#webgl')
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
