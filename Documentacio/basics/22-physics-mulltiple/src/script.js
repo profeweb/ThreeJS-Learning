50585f829c58ed77b250e3e2c829a994de69b6d7
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-02-10 14:52:58 +0100

    ThreeJS

---
+++ Documentacio/basics/22-physics-mulltiple/src/script.js
@@ -0,0 +1,348 @@
+ import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+ // (1) Importar la llibreria Cannon.js
+ import CANNON from 'cannon'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+ const debugObject = { }
+ debugObject.createSphere = ()=>{
+    console.log('Nova Esfera')
+     createSphere(
+         Math.random()* 0.5,
+         {
+             x: (Math.random() - 0.5) * 3,
+             y: 3,
+             z:(Math.random() - 0.5) * 3,
+         })
+ }
+ gui.add(debugObject, 'createSphere')
+
+ debugObject.createBox = ()=>{
+     console.log('Nova Capsa')
+     createBox(
+         Math.random(),
+         Math.random(),
+         Math.random(),
+         {
+             x: (Math.random() - 0.5) * 3,
+             y: 3,
+             z:(Math.random() - 0.5) * 3,
+         })
+ }
+ gui.add(debugObject, 'createBox')
+
+ debugObject.reset = ()=>{
+    console.log(`RESET`)
+     for(const object of objectsToUpdate){
+
+         // Remove body
+         object.body.removeEventListener('collide', playHitSound)
+         world.removeBody(object.body)
+
+         // Remove mesh
+         scene.remove(object.mesh)
+     }
+
+     objectsToUpdate.splice(0, objectsToUpdate.length)
+ }
+gui.add(debugObject, 'reset')
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png'
+])
+
+ // (2) Entorn Físic (CANNON.JS) //////////////////////////////////////////////
+
+ // (2.1) Món físic amb gravetat (World)
+ const world = new CANNON.World()
+ world.gravity.set(0, -9.82, 0)
+
+ // (2.3) Materials del món físic
+
+ const defaultMaterial = new CANNON.Material('default')
+ const defaultContactMaterial = new CANNON.ContactMaterial(
+     defaultMaterial,
+     defaultMaterial,
+     {
+         friction: 0.1,
+         restitution: 0.7
+     }
+ )
+ world.addContactMaterial(defaultContactMaterial)
+ world.defaultContactMaterial = defaultContactMaterial
+
+
+ // (2.4) Afegir el terra al món físic
+ const floorShape = new CANNON.Plane()
+ const floorBody = new CANNON.Body()
+ floorBody.mass = 0
+ //floorBody.material = concreteMaterial
+ //floorBody.material = defaultMaterial
+ floorBody.addShape(floorShape)
+ floorBody.quaternion.setFromAxisAngle(
+     new CANNON.Vec3(1,0,0),
+     -Math.PI*0.5
+ )
+ world.addBody(floorBody)
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+ // (3) Creació d'Esferes dinàmiques
+
+ // (3.3) Array d'objectes dek món físic
+ const objectsToUpdate = []
+
+// (3.5) Extreure constant del bucle
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+     metalness: 0.3,
+     roughness: 0.4,
+     envMap: environmentMapTexture
+ })
+
+ // (3.1) Definició de la funció
+ const createSphere = (radius, position)=>{
+
+    // Mesh (Geometry + Material) de THREE.JS
+    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+     mesh.scale.set(radius, radius, radius)
+     mesh.castShadow = true
+     mesh.position.copy(position)
+     scene.add(mesh)
+
+     // Objecte (Body + Sphere) de CANNON.JS
+     const shape = new CANNON.Sphere(radius)
+     const body = new CANNON.Body({
+         mass: 1,
+         position: new CANNON.Vec3(0, 0, 0),
+         shape,
+         material: defaultMaterial
+     })
+     body.position.copy(position)
+     // Listener de l'event collide
+     body.addEventListener('collide', playHitSound)
+     world.addBody(body)
+
+     objectsToUpdate.push({
+         mesh: mesh,
+         body: body
+     })
+ }
+
+ // (3.2) Cridades a la funció
+ //createSphere(0.5, {x:0, y:3, z:0})
+ //createSphere(0.5, {x:2, y:3, z:0})
+ //createSphere(0.5, {x:-2, y:3, z:0})
+
+ //setInterval(function () {createSphere(0.5, {x:0, y:3, z:0}) }, 1000);
+
+ // (3.4) Test de l'array
+ console.log(objectsToUpdate)
+
+ // (4) Crear capses
+
+ // (4.1) Geometria i material de les capses
+ const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
+ const boxMaterial = new THREE.MeshStandardMaterial({
+     metalness: 0.3,
+     roughness: 0.4,
+     envMap: environmentMapTexture
+ })
+
+ // (4.2) Definició de la funció per crear capses
+ const createBox = (width, height, depth, position)=>{
+
+     // Mesh (Geometry + Material) de THREE.JS
+     const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+     mesh.scale.set(width, height, depth)
+     mesh.castShadow = true
+     mesh.position.copy(position)
+     scene.add(mesh)
+
+     // Objecte (Body + Box) de CANNON.JS
+     const shape = new CANNON.Box( new CANNON.Vec3(width/2, height/2, depth/2))
+     const body = new CANNON.Body({
+         mass: 1,
+         position: new CANNON.Vec3(0, 3, 0),
+         shape,
+         material: defaultMaterial
+     })
+     body.position.copy(position)
+     // Listener de l'event collide
+     body.addEventListener('collide', playHitSound)
+     world.addBody(body)
+
+     objectsToUpdate.push({
+         mesh: mesh,
+         body: body
+     })
+ }
+
+ // (5) Optimització ////////////////////////////////
+ // (5.1) Broadphase
+ world.broadphase = new CANNON.SAPBroadphase(world)
+
+ // (5.2) Sleep
+ world.allowSleep = true
+
+ // (6) EVENTS /////////////////////////////////////
+
+ // (6.1) Audio a reproduir en cas de Col·lisions
+
+ const hitSound = new Audio('sounds/hit.mp3')
+
+ const playHitSound = (collision)=> {
+
+     console.log(collision.contact.getImpactVelocityAlongNormal())
+
+     const impactStrength = collision.contact.getImpactVelocityAlongNormal()
+
+     if(impactStrength>1.5 && !hitSound.isPlaying) {
+         hitSound.volume = Math.random()
+         hitSound.currentTime = 0
+         hitSound.play()
+     }
+ }
+
+ // (6.2) Escoltar l'esdeveniment de col·lisió 'collide'
+
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(- 3, 3, 3)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+ let oldElapsedTime = 0
+
+const tick = () =>
+{
+    // Temps entre fotogrames consecutius
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+    //console.log(deltaTime)
+
+    // (2.2) Update Món Físic
+    world.step(1/60, deltaTime, 3)
+
+    // Update dels objectes
+    for(const object of objectsToUpdate){
+        object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
+    }
+
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Crida a tick a cada frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
--- Documentacio/basics/22-physics/src/script.js
+++ Documentacio/basics/22-physics/src/script.js
@@ -1,7 +1,13 @@
-import './style.css'
+ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+
+ // (1) Importar la llibreria Cannon.js
+ import CANNON from 'cannon'
+
+ // (1.1) Testeam la importació
+ console.log(CANNON)
 
 /**
  * Debug
@@ -31,6 +37,77 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.png',
     '/textures/environmentMaps/0/nz.png'
 ])
+
+ // (2) Entorn Físic (CANNON.JS) //////////////////////////////////////////////
+
+ // (2.1) Món físic amb gravetat (World)
+ const world = new CANNON.World()
+ world.gravity.set(0, -9.82, 0)
+
+ // (4) Materials del món físic
+
+ // (4.1) Materials Ciment i Pàstic
+ const concreteMaterial = new CANNON.Material('concrete')
+ const plasticMaterial  = new CANNON.Material('plastic')
+
+ const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+     concreteMaterial,
+     plasticMaterial,
+     {
+         friction: 0.1,
+         restitution: 0.7
+     }
+ )
+
+ world.addContactMaterial(concretePlasticContactMaterial)
+
+ // (4.2) Material per Defecte
+ const defaultMaterial = new CANNON.Material('default')
+ const defaultContactMaterial = new CANNON.ContactMaterial(
+     defaultMaterial,
+     defaultMaterial,
+     {
+         friction: 0.1,
+         restitution: 0.7
+     }
+ )
+ world.addContactMaterial(defaultContactMaterial)
+
+ // (4.3) Opció simple més simple, aplicar a tot el mateix material)
+ world.defaultContactMaterial = defaultContactMaterial
+
+
+ // (2.2) Afegir objectes (Body + Shape) dins el món físic (World)
+ const sphereShape = new CANNON.Sphere(0.5)
+ const sphereBody = new CANNON.Body({
+     mass: 1,
+     position: new CANNON.Vec3(0,3, 0),
+     shape: sphereShape,
+     //material: plasticMaterial
+     //material: defaultMaterial
+ })
+ world.addBody(sphereBody)
+
+ // (3) Afegir el terra al món físic
+ const floorShape = new CANNON.Plane()
+ const floorBody = new CANNON.Body()
+ floorBody.mass = 0
+ //floorBody.material = concreteMaterial
+ //floorBody.material = defaultMaterial
+ floorBody.addShape(floorShape)
+ floorBody.quaternion.setFromAxisAngle(
+     new CANNON.Vec3(1,0,0),
+     -Math.PI*0.5
+ )
+ world.addBody(floorBody)
+
+// (5) Aplicar forces ////////////////////////////////
+
+ //(5.1) Aplicar puntualment una força a l'esfera
+sphereBody.applyLocalForce(
+    new CANNON.Vec3(150, 0, 0),
+    new CANNON.Vec3(0,0,0)
+)
 
 /**
  * Test sphere
@@ -132,10 +209,25 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+ let oldElapsedTime = 0
 
 const tick = () =>
 {
+    // Temps entre fotogrames consecutius
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+    //console.log(deltaTime)
+
+    // (5.2) Aplicar força per frenar l'esfera (vent)
+    sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position)
+
+    // (2.3) Update Món Físic
+    world.step(1/60, deltaTime, 3)
+    console.log(sphereBody.position.y)
+
+    // (2.4) Update objectes ThreeJS d'acord amb CannonJS
+    sphere.position.copy(sphereBody.position)
 
     // Update controls
     controls.update()
@@ -143,7 +235,7 @@ const tick = () =>
     // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
+    // Crida a tick a cada frame
     window.requestAnimationFrame(tick)
 }
 
