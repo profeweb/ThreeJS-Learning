b5d1fde3eaafe7bfbbc629b8f07a97f1de7de520
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-02-17 11:10:43 +0100

    ThreeJS

---
+++ Documentacio/basics/27b-shaders-not-raw/src/shaders/test/fragment.glsl
@@ -0,0 +1,21 @@
+//Varying
+//varying float vRandom;
+varying vec2 vUv;
+varying float vElevation;
+
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+void main() {
+
+    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    //gl_FragColor = vec4(uColor, 1.0);
+
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb += (uColor*0.5);
+    textureColor.rgb *= vElevation*2.0 + 0.5;
+    gl_FragColor = vec4(textureColor);
+
+    //gl_FragColor = vec4(0.5, vRandom, 1.0, 1.0);
+    //gl_FragColor = vec4(vRandom, vRandom*0.5, 1.0, 1.0);
+}
