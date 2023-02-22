c5f2558bc33af7f260e47b5b6ddb3399cbec0b57
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-02-22 12:05:18 +0100

    ThreeJS

--- Documentacio/basics/28a-shader-patterns-15primers/src/shaders/test/fragment.glsl
+++ Documentacio/basics/28a-shader-patterns-15primers/src/shaders/test/fragment.glsl
@@ -3,61 +3,63 @@ varying vec2 vUv;
 
 void main()
 {
-    // Pattern 1
+    // // Pattern 1
     // gl_FragColor = vec4(vUv, 1.0, 1.0);
 
-    // Pattern 2
+    // // Pattern 2
     // gl_FragColor = vec4(vUv, 0.0, 1.0);
 
-    // Pattern 3
+    // // Pattern 3
     // float strength = vUv.x;
 
-    // Pattern 4
+    // // Pattern 4
     // float strength = vUv.y;
 
-    // Pattern 5
+    // // Pattern 5
     // float strength = 1.0 - vUv.y;
 
-    // Pattern 6
+    // // Pattern 6
     // float strength = vUv.y * 10.0;
 
-    // Pattern 7
+    // // Pattern 7
     // float strength = mod(vUv.y * 10.0, 1.0);
 
-    // Pattern 8
+    // // Pattern 8
     // float strength = mod(vUv.y * 10.0, 1.0);
     // strength = step(0.5, strength);
 
-    // Pattern 9
+    // // Pattern 9
     // float strength = mod(vUv.y * 10.0, 1.0);
     // strength = step(0.8, strength);
 
-    // Pattern 10
+    // // Pattern 10
     // float strength = mod(vUv.x * 10.0, 1.0);
     // strength = step(0.8, strength);
 
-    // Pattern 11
+    // // Pattern 11
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength += step(0.8, mod(vUv.y * 10.0, 1.0));
     // strength = clamp(strength, 0.0, 1.0);
 
-    // Pattern 12
+    // // Pattern 12
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    // Pattern 13
+    // // Pattern 13
     // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
     // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    // Pattern 14
+    // // Pattern 14
     // float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
     // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0, 1.0));
     // float strength = barX + barY;
     // strength = clamp(strength, 0.0, 1.0);
 
-    // Pattern 15
+    // // Pattern 15
      float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
      float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
      float strength = barX + barY;
      strength = clamp(strength, 0.0, 1.0);
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
