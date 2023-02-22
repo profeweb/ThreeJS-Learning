4e4181b7f3798446cb16569200449c66b3e2fb55
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-02-22 10:44:52 +0100

    ThreeJS

--- Documentacio/basics/29-raging-sea/src/script.js
+++ Documentacio/basics/29-raging-sea/src/script.js
@@ -3,11 +3,20 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
+// (2) Importat els shaders
+import waterVertexShader from './shaders/water/vertex.glsl'
+import waterFragmentShader from './shaders/water/fragment.glsl'
+
+// (3) Test de la importació
+//console.log(waterVertexShader)
+//console.log(waterFragmentShader)
+
 /**
  * Base
  */
-// Debug
+// UI Debug
 const gui = new dat.GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,11 +28,65 @@ const scene = new THREE.Scene()
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
 
-// Material
-const waterMaterial = new THREE.MeshBasicMaterial()
+// Colors
+debugObject.depthColor = '#186691'
+debugObject.surfaceColor = '#9bd8ff'
 
+// (1) Material ShaderMaterial
+// (1.1) Configurar tots els seus paràmetres
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+    uniforms:
+        {
+            uTime: {value: 0 },  // Animació
+
+            // Altura i Frequència de les ones
+            uBigWavesElevation: { value: 0.2 },
+            uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+
+            uBigWavesSpeed: { value: 0.75 }, // Velocitat de les ones
+
+            // Colors de les ones (depén d'altura)
+            uDepthColor: {value: new THREE.Color(debugObject.depthColor) },
+            uSurfaceColor: {value: new THREE.Color(debugObject.surfaceColor) },
+
+            // Controls dels colors
+            uColorOffset: { value: 0.08 },
+            uColorMultiplier: { value: 5 }
+
+            // Controls ones petites
+            /*
+                        uSmallWavesElevation: { value: 0.15 },
+                        uSmallWavesFrequency: { value: 3 },
+                        uSmallWavesSpeed: { value: 0.2 },
+                        uSmallIterations: { value: 4 },
+
+
+                         */
+        }
+})
+
+// Controls GUI
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
+
+gui.addColor(debugObject, 'depthColor').onChange(() => { waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor) })
+gui.addColor(debugObject, 'surfaceColor').onChange(() => { waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor) })
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
+
+/*
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallIterations, 'value').min(0).max(5).step(1).name('uSmallIterations')
+
+*/
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
 water.rotation.x = - Math.PI * 0.5
@@ -81,6 +144,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Water
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
---
+++ Documentacio/basics/29-raging-sea/src/shaders/water/fragment.glsl
@@ -0,0 +1,29 @@
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+varying float vElevation;
+
+void main()
+{
+
+    // Mateix color per tots els fragments
+    //gl_FragColor = vec4(0.5, 0.8, 1.0, 1.0);
+
+    // Mescla per iguals entre colors
+    //float mixStrength = 0.5;
+
+    // Mescla segon elevació
+    //float mixStrength = vElevation;
+
+    // Mescla segons elevació i controls (desplaça i amplifica) de colors
+    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+
+    // Visualitza l'elevació
+    //gl_FragColor = vec4(vElevation, vElevation, vElevation, 1.0);
+
+    gl_FragColor = vec4(color, 1.0);
+
+}
