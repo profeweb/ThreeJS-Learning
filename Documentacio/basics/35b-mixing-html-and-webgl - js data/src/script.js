e9df58ac8ebaa326d2b11a1a398646eb6b300c24
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-03-08 13:09:27 +0100

    ThreeJS

---
+++ Documentacio/basics/35b-mixing-html-and-webgl - js data/src/script.js
@@ -0,0 +1,319 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from 'gsap'
+
+/**
+ * Loaders
+ */
+const loadingBarElement = document.querySelector('.loading-bar')
+
+let sceneReady = false
+let pointsReady = false
+
+const loadingManager = new THREE.LoadingManager(
+    // Carregat
+    () =>
+    {
+        // Esperar un moment
+        window.setTimeout(() =>
+        {
+            // Anima overlay
+            gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0, delay: 1 })
+
+            // Update loadingBarElement
+            loadingBarElement.classList.add('ended')
+            loadingBarElement.style.transform = ''
+        }, 500)
+
+        window.setTimeout(() => {
+            sceneReady = true
+        }, 2000)
+    },
+
+    // Progrés
+    (itemUrl, itemsLoaded, itemsTotal) =>
+    {
+        // Calcula el progrés i actaulitza la loadingBarElement
+        const progressRatio = itemsLoaded / itemsTotal
+        loadingBarElement.style.transform = `scaleX(${progressRatio})`
+    }
+)
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
+
+/**
+ * Base
+ */
+// Debug
+const debugObject = {}
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    // wireframe: true,
+    transparent: true,
+    uniforms:
+    {
+        uAlpha: { value: 1 }
+    },
+    vertexShader: `
+        void main()
+        {
+            gl_Position = vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+
+        void main()
+        {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
+
+/**
+ * Update de tots els materials
+ */
+const updateAllMaterials = () =>
+{
+    scene.traverse((child) =>
+    {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+        {
+            // child.material.envMap = environmentMap
+            child.material.envMapIntensity = debugObject.envMapIntensity
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
+}
+
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+
+environmentMap.encoding = THREE.sRGBEncoding
+
+scene.background = environmentMap
+scene.environment = environmentMap
+
+debugObject.envMapIntensity = 2.5
+
+/**
+ * Models
+ */
+gltfLoader.load(
+    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
+    (gltf) =>
+    {
+        gltf.scene.scale.set(2.5, 2.5, 2.5)
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
+
+        updateAllMaterials()
+    }
+)
+
+// (3) Definir el Raycaster
+const raycaster = new THREE.Raycaster()
+
+// (1) Definir l'array de unts d'interés
+const points = [
+    {
+        position: new THREE.Vector3(1.55, 0.3, - 0.6),
+        element: null,
+        text: "Front and top screen with HUD aggregating terrain and battle informations.",
+        number: 0
+    },
+    {
+        position: new THREE.Vector3(0.5, 0.8, - 1.6),
+        element: null,
+        text: "Ventilation with air purifier and detection of environment toxicity.",
+        number: 1
+    },
+    {
+        position: new THREE.Vector3(1.6, - 1.3, - 0.7),
+        element: null,
+        text: "Cameras supporting night vision and heat vision with automatic adjustment.",
+        number: 2
+    }
+]
+
+function addPointstoHTML(){
+    for(const point of points){
+
+        const newPointDiv = document.createElement("div");
+        newPointDiv.setAttribute('class', 'point point-'+point.number)
+        point.element = newPointDiv
+
+        const newLabelDiv = document.createElement("div");
+        newLabelDiv.setAttribute('class', 'label')
+        newLabelDiv.innerText = point.number
+        newPointDiv.appendChild(newLabelDiv)
+
+        const newTextDiv = document.createElement("div");
+        newTextDiv.setAttribute('class', 'text')
+        newTextDiv.innerText = point.text
+        newPointDiv.appendChild(newTextDiv)
+
+
+        document.querySelector('body').appendChild(newPointDiv)
+    }
+
+    pointsReady = true
+}
+
+addPointstoHTML()
+
+// console.log(points)
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 3, - 2.25)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(4, 1, - 4)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 3
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animar
+ */
+const tick = () =>
+{
+    // Update controls
+    controls.update()
+
+    // (2) Actualitzar la posició del punts dins l'escene
+
+    // Update points quan l'escena està completament carregada
+    if(sceneReady && pointsReady)
+    {
+        // (2.1) Recorre cada punt de l'array
+        for(const point of points)
+        {
+            // (2.2) Obtenir la posició 2D de la pantalla
+            const screenPosition = point.position.clone()
+            screenPosition.project(camera)
+    
+            // (3.1) Tirar el raycaster
+            raycaster.setFromCamera(screenPosition, camera)
+            const intersects = raycaster.intersectObjects(scene.children, true)
+    
+            // (3.2) Si no hi ha interseccions
+            if(intersects.length === 0)
+            {
+                // Fer visible
+                point.element.classList.add('visible')
+            }
+
+            // (3.3) Intersecció trobada
+            else
+            {
+                // Calcular la distància entre la intersecció i la distància del punt
+                const intersectionDistance = intersects[0].distance
+                const pointDistance = point.position.distanceTo(camera.position)
+    
+                // Intersecció és més a prop que el punt
+                if(intersectionDistance < pointDistance)
+                {
+                    // Amagar
+                    point.element.classList.remove('visible')
+                }
+                // Intersecció és més lluny que el punt
+                else
+                {
+                    // Mostrar
+                    point.element.classList.add('visible')
+                }
+            }
+
+            // Actualitza la posició del punt
+            const translateX = screenPosition.x * sizes.width * 0.5
+            const translateY = - screenPosition.y * sizes.height * 0.5
+            point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
+        }
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Cridar a la funció tick a cada fotograma
+    window.requestAnimationFrame(tick)
+}
+
+tick()
